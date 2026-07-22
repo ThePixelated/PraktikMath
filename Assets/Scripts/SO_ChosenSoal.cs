52ad0b58abad177fb1423d5a27ae272d59d84529
ThePixelated <radimastriaputra@gmail.com>
2026-07-22 21:49:05 +0700

    cleanup

--- Assets/Scripts/SO_ChosenSoal.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ChosenSoal", menuName = "Scriptable Objects/ChosenSoal")]
-public class SO_ChosenSoal : ScriptableObject
-{
-    public SO_SoalData ChosenSoal;
-}
